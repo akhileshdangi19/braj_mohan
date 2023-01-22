68e58449b88040cee4d33a45b573929fa421dcc8
akhileshdangi19 <akhileshdangi19@gmail.com>
2023-01-22 17:12:53 +0530

    new-project

---
+++ index.js
@@ -0,0 +1,108 @@
+// console.log("namste js")
+
+// const obj ={
+//     fn:function(){
+//         console.log(this);
+//     },
+//     fn2:()=>{
+//         console.log(this )
+//     },
+// }
+// obj.fn() //obj
+// obj.fn2() //window
+
+
+// const  person ={
+//     name:"Akshay ",
+//     print:function(){
+//         console.log(this)
+//     }
+// }
+// const person2 ={
+//     name: "simran"
+// }
+// const person2={
+//     name:"simran",
+// }
+// person.print();
+// person.print.call()
+// person.print.call(this)
+// person.print.call(person2);
+// person.print.call(person)
+
+
+// call apply and bind
+
+// call
+// function.prototype.call()
+//
+
+// const  okbj ={
+
+// FirstName:"Akshay",
+// printName:()=>{
+//     console.log(this.FirstName)
+// }
+// }
+
+// const obj2={
+//     FirstName:"simran"
+// }
+// obj2.printName()
+// obj2.printName()
+
+// Interviews Tips
+
+/**
+ * 
+ * 
+ * Luck Is very Importent for Interview
+ * 
+ * Most companies dont' train thier interviwers 
+ * 
+ * A person can be a good software Engineer but a Bad interviwer
+ * 
+ * LUCK - Can't controll
+ * 
+ * Bad Interviwer
+ * 
+ * waht we can control->
+ * 
+ * Our [prepartion]=> More than 5 day's 
+ * -(50%)  technical  skill's - 
+ *   - everyone Knows thier own way's two prepare
+ * what is other major thing 
+ * -[50%]  communicatiin skill's- 
+ * - speak your thought's   while you code *
+ *   - practice to speak even when you're coding alone 
+ * 
+ * - if you can't explain,
+ *  then '(interviewer thinks) you' domnt'know
+ * - you don't work alone in this company 
+ * - companies expect you to have good 
+ *    communicatiion skills
+ * it's a brutal truth, you should know english
+ * 
+ * 
+ * other types of preparation ->
+ * mocks interviews
+ * (preparation on the interview day)
+ * -you should not be hustlinng at last moment and panic mode.
+ * 
+ * -pen/paper's ,
+ * water,
+ * botal, 
+ * keep you'r camera around 
+ * keep you'r phone on slint's
+ * routr ups 
+ * 
+ * CONFIDENT COME FROM PREPARATION 
+ * (not just your technical preparation)
+ * 
+ * chat gpt 
+ *  
+ */
+
+
+
+
---
+++ test.js
@@ -0,0 +1,33 @@
+// Arrow function
+
+function x(){
+    const a =10;
+
+}
+
+var xyz = 30;
+
+x(); //functional exicution context is created 
+
+var x =function(){
+    console.log("I am an anonymous function")
+}
+
+
+// data structure for storing data heap 
+// anonymous
+ function test (x){
+    return function(){
+
+    }
+ }
+
+
+//  in es6 in Arrow function
+
+// promis , spread opertor, this
+// this arrow is also known as a flat arrow
+const fn=()=>{
+// it take this for it's lexical parrent 
+
+}
